11d3877946b2abc5e553c1e1499d92db76659362
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-05-23 02:07:00 +0300

    added contact page and make mobile responsive

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import './src/style.css'
 import './output.css'
 import * as THREE from 'three'
+
+
+var contact = document.getElementById('contact')
+contact.addEventListener('click', () => {
+  var contactplace = document.getElementById('contact-me')
+  contactplace.scrollIntoView({ behavior: "smooth" })
+})
+
+
 const scence = new THREE.Scene()
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -36,6 +45,15 @@ const baraa = new THREE.Mesh(
   new THREE.BoxGeometry(9, 9, 9),
   new THREE.MeshBasicMaterial({ map: Baraatexture })
 )
+var baraaX
+var baraaY
+if (innerWidth > 900) {
+  baraaX = 20
+  baraaY = 0
+} else {
+  baraaX = 0
+  baraaY = -20
+}
 baraa.position.x = 20
 baraa.position.y = 0
 scence.add(baraa)
@@ -46,15 +64,6 @@ scence.add(baraa)
 Array(2000).fill().forEach(addstar)
 
 var skillcubelist = []
-var skillY
-if (innerWidth < 400) {
-  skillY = 0
-} else if (innerWidth < 800) {
-  skillY = 5
-}
-else {
-  skillY = 10
-}
 function createskillcube(imgurl, shape, x, y, z) {
   const textureLoader = new THREE.TextureLoader()
   textureLoader.crossOrigin = "Anonymous"
@@ -64,20 +73,48 @@ function createskillcube(imgurl, shape, x, y, z) {
     new THREE.MeshBasicMaterial({ map: myTexture })
   )
   skillcubelist = [...skillcubelist, skillcube]
-  skillcube.position.x = x + skillY * 3
-  skillcube.position.y = y - skillY
+  skillcube.position.x = x
+  skillcube.position.y = y
   skillcube.position.z = z
   scence.add(skillcubelist[skillcubelist.length - 1])
 }
-createskillcube('https://i.ibb.co/tPVDQ4T/tailwindcssicon.png', new THREE.CylinderGeometry(3, 3, 3), -50, -130, 0)
-createskillcube('https://i.ibb.co/fMYyjnT/html-icon.png', new THREE.BoxGeometry(4, 4, 4), -70, -140, 0)
-createskillcube('https://i.ibb.co/68ybrXD/next-icon.png', new THREE.BoxGeometry(4, 4, 4), 0, -140, 0)
-createskillcube('https://i.ibb.co/Bt3QyMp/svelte-icon.png', new THREE.BoxGeometry(4, 4, 4), -60, -120, 0)
-createskillcube('https://i.ibb.co/WkxYJ0S/github-icon.png', new THREE.BoxGeometry(4, 4, 4), -30, -120, 0)
-createskillcube('https://i.ibb.co/r5LRCmY/js-icon.png', new THREE.BoxGeometry(4, 4, 4), -40, -150, 0)
-createskillcube('https://i.ibb.co/THPh4NV/react-icon.png', new THREE.BoxGeometry(8, 8, 8), -20, -150, 0)
-createskillcube('https://i.ibb.co/JzV3NgH/css-icon.png', new THREE.BoxGeometry(4, 4, 4), -20, -125, 0)
+if (innerWidth < 800) {
+  createskillcube('https://i.ibb.co/tPVDQ4T/tailwindcssicon.png',
+    new THREE.CylinderGeometry(3, 3, 3), 0, -110, 0)
+  createskillcube('https://i.ibb.co/fMYyjnT/html-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 70, -120, 0)
+  createskillcube('https://i.ibb.co/68ybrXD/next-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 10, -120, 0)
+  createskillcube('https://i.ibb.co/Bt3QyMp/svelte-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), -10, -130, 0)
+  createskillcube('https://i.ibb.co/WkxYJ0S/github-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 0, -130, 0)
+  createskillcube('https://i.ibb.co/r5LRCmY/js-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 10, -130, 0)
+  createskillcube('https://i.ibb.co/THPh4NV/react-icon.png',
+    new THREE.BoxGeometry(8, 8, 8), 10, -140, 0)
+  createskillcube('https://i.ibb.co/JzV3NgH/css-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 10, -115, 0)
 
+} else {
+  createskillcube('https://i.ibb.co/tPVDQ4T/tailwindcssicon.png',
+    new THREE.CylinderGeometry(3, 3, 3), -20, -140, 0)
+  createskillcube('https://i.ibb.co/fMYyjnT/html-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), -40, -150, 0)
+  createskillcube('https://i.ibb.co/68ybrXD/next-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 30, -150, 0)
+  createskillcube('https://i.ibb.co/Bt3QyMp/svelte-icon.png'
+    , new THREE.BoxGeometry(4, 4, 4), -30, -130, 0)
+  createskillcube('https://i.ibb.co/WkxYJ0S/github-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 0, -130, 0)
+  createskillcube('https://i.ibb.co/r5LRCmY/js-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), -10, -160, 0)
+  createskillcube('https://i.ibb.co/THPh4NV/react-icon.png',
+    new THREE.BoxGeometry(8, 8, 8), 10, -160, 0)
+  createskillcube('https://i.ibb.co/JzV3NgH/css-icon.png',
+    new THREE.BoxGeometry(4, 4, 4), 10, -135, 0)
+
+}
 
 const spaceTexture = new THREE.TextureLoader().load('https://i.ibb.co/X3Vj5G7/space.jpg')
 scence.background = spaceTexture;
@@ -117,7 +154,48 @@ document.body.onscroll = moveCamera
 var Ypos
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
-
+  if (innerWidth > 900) {
+    baraa.position.x = 20
+    baraa.position.y = 0
+  } else {
+    baraa.position.x = 0
+    baraa.position.y = -20
+  }
+  if (innerWidth < 800) {
+    skillcubelist[0].position.y = -110
+    skillcubelist[1].position.y = -120
+    skillcubelist[2].position.y = -120
+    skillcubelist[3].position.y = -130
+    skillcubelist[4].position.y = -130
+    skillcubelist[5].position.y = -130
+    skillcubelist[6].position.y = -140
+    skillcubelist[7].position.y = -115
+    skillcubelist[0].position.x = 70
+    skillcubelist[1].position.x = 0
+    skillcubelist[2].position.x = 10
+    skillcubelist[3].position.x = -10
+    skillcubelist[4].position.x = 0
+    skillcubelist[5].position.x = 10
+    skillcubelist[6].position.x = 10
+    skillcubelist[7].position.x = 10
+  } else {
+    skillcubelist[0].position.y = -140
+    skillcubelist[1].position.y = -150
+    skillcubelist[2].position.y = -150
+    skillcubelist[3].position.y = -130
+    skillcubelist[4].position.y = -130
+    skillcubelist[5].position.y = -160
+    skillcubelist[6].position.y = -160
+    skillcubelist[7].position.y = -135
+    skillcubelist[0].position.x = -20
+    skillcubelist[1].position.x = -40
+    skillcubelist[2].position.x = 30
+    skillcubelist[3].position.x = -30
+    skillcubelist[4].position.x = 0
+    skillcubelist[5].position.x = -10
+    skillcubelist[6].position.x = 10
+    skillcubelist[7].position.x = 10
+  }
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
